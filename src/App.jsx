c474f42a660c02ef15767a89b62aee7a665b7d10
emimrulkayes <emimrulkayes@gmail.com>
2023-10-31 03:29:01 +0600

    Added actions button and change  file name & structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,13 @@
 import '../assets/css/timer.css';
-import React, { useEffect, useState, useRef } from 'react';
+import React from 'react';
+import AkTimer from './AkTimer';
 
 function App() {
 
-  const [timerDays, setTimerDays] = useState('00');
-  const [timerHours, setTimerHours] = useState('00');
-  const [timerMinutes, setTimerMinutes] = useState('00');
-  const [timerSeconds, setTimerSeconds] = useState('00');
-
-  let interval = useRef();
-
-  const startTimer = () => {
-      const countdownDate = new Date('November 30, 2023 00:00:00').getTime()
-
-      interval = setInterval(() => {
-          const now = new Date().getTime();
-          const distance = countdownDate - now;
-
-          const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-          const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
-          const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-          const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-          if (distance < 0) {
-              console.log('Stop the timer')
-          } else {
-              setTimerDays(days);
-              setTimerHours(hours);
-              setTimerMinutes(minutes);
-              setTimerSeconds(seconds);
-          }
-
-      }, 1000);
-  };
-
-  useEffect(() => {
-      startTimer();
-      return () => {
-          clearInterval(interval.current);
-      };
-  });
-
   return (
-    
-    <section className='timer-container'> 
-      <section className='timer'> 
-        <div>
-          <span className="mdi mdi-calendar-clock timer-icon"></span>
-          <h2>AK Timer</h2>
-          <p>Timer to a really special date. One you could or would never amagine.</p>
-        </div>
-        <div className='timer-box'>
-          <section>
-            <p>{timerDays}</p>
-            <p><small>Days</small></p>
-          </section>
-          <span>:</span>
-          <section>
-            <p>{timerHours}</p>
-            <p><small>Hours</small></p>
-          </section>
-          <span>:</span>
-          <section>
-            <p>{timerMinutes}</p>
-            <p><small>Minutes</small></p>
-          </section>
-          <span>:</span>
-          <section>
-            <p>{timerSeconds}</p>
-            <p><small>Scounds</small></p>
-          </section>
-        </div>
-
-      </section>
-    </section>
+    <> 
+      <AkTimer />
+    </>
   )
 }
 
